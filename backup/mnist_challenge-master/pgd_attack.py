f44409b98f6c55fd2ef99a2607a42eba244240b5
Fulvioserao99 <120859540+Fulvioserao99@users.noreply.github.com>
2023-04-20 01:19:48 +0200

    Add files via upload

---
+++ backup/mnist_challenge-master/pgd_attack.py
@@ -0,0 +1,133 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import numpy as np
+import json
+
+# Classe per l'attacco LinfPGD
+class LinfPGDAttack:
+  def __init__(self, model, epsilon, k, a, random_start, loss_func):
+    """Inizializzazione dei parametri di attacco. L'attacco esegue k passi
+       di dimensione a, rimanendo sempre entro epsilon dal punto iniziale."""
+    self.model = model
+    self.epsilon = epsilon
+    self.k = k
+    self.a = a
+    self.rand = random_start
+
+    if loss_func == 'xent':
+      self.loss = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+    elif loss_func == 'cw':
+      label_mask = tf.one_hot(model.y_input,
+                              10,
+                              on_value=1.0,
+                              off_value=0.0,
+                              dtype=tf.float32)
+      correct_logit = tf.reduce_sum(label_mask * model.pre_softmax, axis=1)
+      wrong_logit = tf.reduce_max((1-label_mask) * model.pre_softmax
+                                  - 1e4*label_mask, axis=1)
+      self.loss = -tf.nn.relu(correct_logit - wrong_logit + 50)
+    else:
+      print('Funzione di perdita sconosciuta. Si utilizza la cross-entropy per default')
+      self.loss = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+      
+  
+  
+  def perturb(self, x_nat, y):
+    """Dato un set di esempi (x_nat, y), restituisce un set di esempi avversari
+       entro epsilon di x_nat in norma l_infinity."""
+    if self.rand:
+      x = tf.Variable(x_nat) + tf.cast(tf.random.uniform(x_nat.shape, -self.epsilon, self.epsilon), tf.float32)
+      x = tf.clip_by_value(x, 0, 1) # assicura un range di pixel valido
+    else:
+      x = tf.Variable(x_nat)
+
+    for i in range(self.k):
+      with tf.GradientTape() as tape:
+        tape.watch(x)
+        loss = self.loss(y, self.model(x))
+      grad = tape.gradient(loss, x)
+
+      x += self.a * tf.sign(grad)
+
+      x = tf.clip_by_value(x, x_nat - self.epsilon, x_nat + self.epsilon) 
+      x = tf.clip_by_value(x, 0, 1) # assicura un range di pixel valido
+
+    return x
+
+
+
+if __name__ == '__main__':
+  import json
+  import sys
+  import math
+
+  
+  from model import Model
+
+  # Caricamento delle impostazioni di configurazione
+  with open('config.json') as config_file:
+    config = json.load(config_file)
+
+
+  model_file = tf.train.latest_checkpoint(config['model_dir'])
+  if model_file is None:
+    print('No model found')
+    sys.exit()
+
+
+  # Caricamento del modello salvato
+  model = Model()
+  attack = LinfPGDAttack(model,
+                         config['epsilon'],
+                         config['k'],
+                         config['a'],
+                         config['random_start'],
+                         config['loss_func'])
+  saver = tf.keras.Model.save_weights(model,'savers/my_model_weights')
+  
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+batch =  config['training_batch_size']
+
+# Caricamento del dataset MNIST
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_train = np.expand_dims(x_train.astype(np.float32) / 255.0, axis=-1)
+y_train = tf.keras.utils.to_categorical(y_train, num_classes=10)
+x_test = np.expand_dims(x_test.astype(np.float32) / 255.0, axis=-1)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes=10)
+
+# Costruzione del modello
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)),
+    tf.keras.layers.MaxPooling2D((2,2)),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2,2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+# Compilazione del modello
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Addestramento del modello
+model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
+
+# Valutazione del modello sul test set
+test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+print('Test accuracy:', test_acc)
+
+# Attacco di LinfPGD sul test set
+attack = LinfPGDAttack(model=model, epsilon=0.1, k=40, a=0.01, random_start=True, loss_func='ce')
+adv_x_test = attack.perturb(x_test, y_test)
+
+# Valutazione del modello sull'insieme di test avversario
+test_loss_adv, test_acc_adv = model.evaluate(adv_x_test, y_test, verbose=2)
+print('Test accuracy on adversarial examples:', test_acc_adv)
+
---
+++ backup/mnist_challenge-master/train.py
@@ -0,0 +1,148 @@
+import tensorflow as tf
+from datetime import datetime
+import json
+import os
+import shutil
+import numpy as np
+from timeit import default_timer as timer
+
+from model import Model
+from pgd_attack import LinfPGDAttack
+
+@tf.function
+def evaluate(model, x, y):
+    predictions = model(x)
+    accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(predictions, axis=1), tf.argmax(y, axis=1)), tf.float32))
+    return accuracy
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+# Setting up training parameters
+tf.random.set_seed(config['random_seed'])
+max_num_training_steps = config['max_num_training_steps']
+num_output_steps = config['num_output_steps']
+num_summary_steps = config['num_summary_steps']
+num_checkpoint_steps = config['num_checkpoint_steps']
+batch_size = config['training_batch_size']
+
+# Setting up the data and the model
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+mnist_train = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(batch_size)
+global_step = tf.Variable(0, trainable=False, dtype=tf.int64)
+model = Model()
+
+# Setting up the optimizer
+optimizer = tf.optimizers.Adam(1e-4)
+
+# Set up adversary
+attack = LinfPGDAttack(model,
+                       config['epsilon'],
+                       config['k'],
+                       config['a'],
+                       config['random_start'],
+                       config['loss_func'])
+
+
+# Setting up the Tensorboard and checkpoint outputs
+model_dir = config['model_dir']
+if not os.path.exists(model_dir):
+  os.makedirs(model_dir)
+
+
+# We add accuracy and loss twice so we can easily make three types of
+# comparisons in Tensorboard:
+# - train vs eval (for a single run)
+# - train of different runs
+# - eval of different runs
+checkpoint = tf.train.Checkpoint(model=model)
+saver = tf.train.CheckpointManager(checkpoint,'savers',max_to_keep=3)
+train_accuracy_adv = tf.keras.metrics.Accuracy()
+test_accuracy_adv = tf.keras.metrics.Accuracy()
+xent_adv_train = tf.keras.metrics.CategoricalCrossentropy()
+xent_adv = tf.keras.metrics.CategoricalCrossentropy()
+'''tf.summary.scalar('accuracy adv train', train_accuracy_adv.result())
+tf.summary.scalar('accuracy adv', test_accuracy_adv.result())
+tf.summary.scalar('xent adv train', xent_adv_train.result())
+tf.summary.scalar('xent adv', xent_adv.result())
+tf.summary.image('images adv train', model.x_image)
+merged_summaries = tf.compat.v1.summary.merge_all()'''
+
+
+@tf.function
+def train_step(x_batch, y_batch):
+    # Initialize training time counter
+    training_time = 0.0
+
+        # Compute Adversarial Perturbations
+    start = timer()
+    x_batch_adv = attack.perturb(x_batch, y_batch)
+    end = timer()
+    training_time += end - start
+
+    nat_dict = {model.x_input: x_batch,
+                model.y_input: y_batch}
+
+    adv_dict = {model.x_input: x_batch_adv,
+                model.y_input: y_batch}
+
+    with tf.GradientTape() as tape:
+        # Compute logits for natural inputs
+        logits_nat = model(nat_dict['x_input'], training=True)
+        # Compute loss for natural inputs
+        loss_nat = tf.keras.losses.sparse_categorical_crossentropy(
+            nat_dict['y_input'], logits_nat, from_logits=True)
+        loss_nat = tf.reduce_mean(loss_nat)
+
+        # Compute logits for adversarial inputs
+        logits_adv = model(adv_dict['x_input'], training=True)
+        # Compute loss for adversarial inputs
+        loss_adv = tf.keras.losses.sparse_categorical_crossentropy(
+            adv_dict['y_input'], logits_adv, from_logits=True)
+        loss_adv = tf.reduce_mean(loss_adv)
+
+        # Compute total loss
+        loss = loss_nat + loss_adv
+
+    # Compute gradients and update weights
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+    # Output to stdout
+    if global_step % num_output_steps == 0:
+        nat_acc = evaluate(model, nat_dict['.x_input'], nat_dict['y_input'])
+        adv_acc = evaluate(model, adv_dict['x_input'], adv_dict['y_input'])
+        print('Step {}:    ({})'.format(global_step, datetime.now()))
+        print('    training nat accuracy {:.4}%'.format(nat_acc * 100))
+        print('    training adv accuracy {:.4}%'.format(adv_acc * 100))
+        if global_step != 0:
+            print('    {} examples per second'.format(
+                num_output_steps * batch_size / training_time))
+            training_time = 0.0
+        # Tensorboard summaries
+        # da aggiungere
+        if global_step % num_checkpoint_steps == 0:
+            model.save(
+                os.path.join(model_dir, 'checkpoint'),
+                save_format='tf',
+                options=tf.saved_model.SaveOptions(experimental_io_device='/job:localhost'))
+    
+    train_accuracy_adv.update_state(tf.argmax(nat_dict['y_input'], axis=1), tf.argmax(logits_nat, axis=1))
+    test_accuracy_adv.update_state(tf.argmax(adv_dict['y_input'], axis=1), tf.argmax(logits_adv, axis=1))
+    xent_adv_train.update_state(loss_nat/batch_size)
+    xent_adv.update_state(loss_adv/batch_size)
+
+    # Update global step
+    global_step.assign_add(1)
+
+training_time = 0
+start = timer()
+for x_batch, y_batch in mnist_train:
+    train_step(x_batch,y_batch)
+
+end = timer()
+
+training_time += end - start
+
